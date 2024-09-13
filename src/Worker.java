1f4e3eaa53af8b6eed61b186db6d51b0322079a7
derossca <derossca@mail.uc.edu>
2024-09-13 08:46:15 -0400

    added all notes

--- src/Worker.java
+++ src/Worker.java
@@ -7,7 +7,17 @@ public class Worker {
 
     /*
     Methods
-    double calculateWeeklyPay(double hoursWorked)
-    String displayWeeklyPay(double hoursWorked)
+    1. double calculateWeeklyPay(double hoursWorked)
+    2. String displayWeeklyPay(double hoursWorked)
+    3. constructor using super() to call constructor for Person
+    then set rest of fields
+
+    hours under 40 are at HourlyRate
+    hourse above 40 are 1.5 time
+
+    displayWeeklyPay should indicate number of hours of reg pay
+    and total and number of hours of overtime to get total combined pay
+
+    overload toCSV(), toXML(), toJSON() to include new data field
      */
 }
