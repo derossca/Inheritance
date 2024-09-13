9bc4d916ac912ad53999f298958db05aee12c51a
derossca <derossca@mail.uc.edu>
2024-09-13 08:42:16 -0400

    added notes to InheritanceDemo

--- src/InheritanceDemo.java
+++ src/InheritanceDemo.java
@@ -1,2 +1,14 @@
 public class InheritanceDemo {
+    public static void main(String[] args) {
+        /*
+        Create 3 workers and 3 salaryWorker instances and add
+        to ArrayList<Worker>
+
+        2. Write a loop that simulates 3 weekly pay periods.
+        Week 1 40 hours
+        Week 2 50 hours
+        Week 3 40 hours
+        show weekly pay for each worker for each week in tabular format
+         */
+    }
 }
