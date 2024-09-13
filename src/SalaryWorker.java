a8f4405d3db45d7e6cf15e1cc651ced509048608
derossca <derossca@mail.uc.edu>
2024-09-13 08:40:06 -0400

    added Worker, SalaryWorker, and InheritanceDemo classes

---
+++ src/SalaryWorker.java
@@ -0,0 +1,18 @@
+public class SalaryWorker {
+    /*
+    Fields
+    double annualSalary
+
+    Methods
+    1. constructor for SalaryWorker using super() to call
+    constructor for Worker
+
+    2. double calculateWeeklyPay(double hoursWorked)
+
+    3. @Override calculateWeekly Pay and displayWeeklyPay
+    calculateWeeklyPay returns pay total
+    take annual salary and divide by 52 to get weekly pay
+
+    4. overload toCSV(), toXML(), toJSON() to include new data field
+     */
+}
---
+++ src/Worker.java
@@ -0,0 +1,13 @@
+public class Worker {
+
+    /*
+    Fields
+    double hourlyPayRate
+     */
+
+    /*
+    Methods
+    double calculateWeeklyPay(double hoursWorked)
+    String displayWeeklyPay(double hoursWorked)
+     */
+}
