79c0b78f41e1ba71afbeead507f1423db93d0a45
derossca <derossca@mail.uc.edu>
2024-09-16 14:15:57 -0400

    started working on junit test for SalaryWorkerTest

---
+++ src/SalaryWorkerTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SalaryWorkerTest {
+
+    SalaryWorker s1;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setAnnualSalary() {
+    }
+
+    @Test
+    void calculateWeeklyPay() {
+    }
+
+    @Test
+    void displayWeeklyPay() {
+    }
+
+    @Test
+    void toCSVDataRecord() {
+    }
+
+    @Test
+    void toJSONDataRecord() {
+    }
+
+    @Test
+    void toXMLDataRecord() {
+    }
+}
