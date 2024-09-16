ece766fff74ec695e685cc9de6baa2f2f705f5a7
derossca <derossca@mail.uc.edu>
2024-09-16 14:21:08 -0400

    working on SalaryWorkerTest
    finished display and calculate tests

--- src/SalaryWorkerTest.java
+++ src/SalaryWorkerTest.java
@@ -8,18 +8,27 @@ class SalaryWorkerTest {
 
     @BeforeEach
     void setUp() {
+        s1 = new SalaryWorker("000001", "John", "Jones", "Esq.", 1990, 50,96000);
     }
 
     @Test
     void setAnnualSalary() {
+        s1.setAnnualSalary(50000);
+        assertEquals(50000, s1.getAnnualSalary());
     }
 
     @Test
     void calculateWeeklyPay() {
+        double expectedPay = s1.calculateWeeklyPay(40);
+        double actualPay = s1.calculateWeeklyPay(40);
+        assertEquals(expectedPay, actualPay);
     }
 
     @Test
     void displayWeeklyPay() {
+        String expectedDisplay = s1.displayWeeklyPay(50);
+        String actualDisplay = s1.displayWeeklyPay(50);
+        assertEquals(expectedDisplay, actualDisplay);
     }
 
     @Test
